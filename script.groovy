4d5fd6304d10d864bdcdef126f21e83921d86f87
Tomer <daniely.tomer@gmail.com>
2022-12-17 19:15:14 +0200

    fix jenkins

--- script.groovy
+++ script.groovy
@@ -46,7 +46,7 @@ def build_for_ecr(){
 def publish_image(){                  
     docker.withRegistry('https://644435390668.dkr.ecr.us-east-1.amazonaws.com', 'ecr:us-east-1:aws_card_push_image') {
     app.push("${env.VERSION}")
-    } 
+    } }
 
 
 def clean_docker(){
