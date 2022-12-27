dcd99f8bc72671c85fcd3a1d1b51c495c8107456
Tomer <daniely.tomer@gmail.com>
2022-12-27 19:53:23 +0200

    add deploy

--- script.groovy
+++ script.groovy
@@ -60,7 +60,7 @@ def update_helm_version(){
         sh "git config --global user.email 'daniely.tomer@gmail.com'"
         sh "git config --global user.name 'tomer'"
         sh "cat chatapp-helm/chatapp/values.yaml"
-        sh "sed s/*.*.*/${env.VERSION}/g chatapp/values.yaml"
+        sh "sed s/*.*.*/${env.VERSION}/g chatapp-helm/chatapp/values.yaml"
         sh "cat chatapp-helm/chatapp/values.yaml"
         sh "git push https://${G_USER}:${G_PASS}@github.com/TomerDan/chatapp-helm.git main"
         //sh "git tag -a ${env.VERSION} -m 'version ${env.VERSION}'"
